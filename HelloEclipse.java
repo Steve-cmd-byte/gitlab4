bc93de6fcae3fc46b42abca4794ec38de7389bd9
Steve Yang <steve199203@gmail.com>
2020-09-26 13:40:25 +0800

    First version

---
+++ HelloEclipse.java
@@ -0,0 +1,13 @@
+package xxx;
+
+import java.util.Scanner;
+
+public class HelloEclipse {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int i = input / 10;
+		System.out.println(i);
+	}
+}
