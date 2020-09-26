fa29a699d127903e4442f9f8c44788f1d6c5d3ad
Steve Yang <steve199203@gmail.com>
2020-09-26 14:38:39 +0800

    Forth version

--- HelloEclipse.java
+++ HelloEclipse.java
@@ -7,7 +7,7 @@ public class HelloEclipse {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
-		int i = input / 10;
+		int i = input / 100;
 		System.out.println(i);
 		System.out.println(i);
 		System.out.println(i);
